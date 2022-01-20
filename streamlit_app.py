5b7864819efa5ddde4bb06286767acacaf47f03e
nikita2026 <nikitabaxi1102@gmail.com>
2022-01-19 23:49:39 -0700

    streamlit_app.py changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,19 @@
 import streamlit as st
+from google.cloud import firestore
 
-st.header('HELLO !!')
-if st.button('Balloons?'):
-    st.balloons()
+#st.header('HELLO !!')
+#if st.button('Balloons?'):
+#   st.balloons()
+
+#Authenticate to Firestore with the JSON account key
+db = firestore.Client.from_service_account_json("firestore-key.json")
+
+#Create a reference to the Google post
+doc_ref = db.collection("posts").document("Google")
+
+#Get data at that reference
+doc = doc_ref.get()
+
+#See the data obtained
+st.write("ID: ", doc.id)
+st.write("Contents: ", doc.to_dict()) 
